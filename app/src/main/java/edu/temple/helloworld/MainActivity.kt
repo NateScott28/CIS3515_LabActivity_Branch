944f20f63df07bc0973c6d724060b045e4be160a
NateScott28 <tul07731@temple.edu>
2024-02-21 15:32:41 -0500

    Name entry error fix
    added fix to prevent people from not entering name

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -20,7 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if(findViewById<EditText>(R.id.nameEditText).text.isEmpty()) {
+                displayTextView.text = "Please enter name"
+            }
+            else {
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
+
         }
 
 
